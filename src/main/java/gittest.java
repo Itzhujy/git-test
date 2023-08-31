9ce1ae71697783868a31959f10fce1c294f20560
zhu <zhu@qq.com>
2023-08-31 22:03:23 +0800

    master commit1

--- src/main/java/gittest.java
+++ src/main/java/gittest.java
@@ -5,5 +5,6 @@ public class gittest {
         System.out.println("hello git!");
         System.out.println("hello hot-fix");
         System.out.println("hello");
+        System.out.println("amagi");
     }
 }
