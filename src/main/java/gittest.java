ea122d14a8ec4cd2496b92d794cd6e2dc3b92602
zhu <zhu@qq.com>
2023-08-31 21:38:21 +0800

    second commit

--- src/main/java/gittest.java
+++ src/main/java/gittest.java
@@ -1,5 +1,7 @@
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("Hello git!");
+        System.out.println("hello git!");
     }
 }
